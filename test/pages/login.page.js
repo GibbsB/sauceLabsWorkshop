adc5519b8d793564b514e97584aa54d3794a4f55
Brandon Gibbons <brandongibbons3506@gmail.com>
2020-03-05 12:34:22 -0600

    Cleanup via prettier

--- test/pages/login.page.js
+++ test/pages/login.page.js
@@ -1,45 +1,45 @@
 class LoginPage {
-    get usernameElem() {
-        return $('#user-name');
-    }
+  get usernameElem() {
+    return $("#user-name");
+  }
 
-    get passwordElem() {
-        return $('#password');
-    }
+  get passwordElem() {
+    return $("#password");
+  }
 
-    get loginButtonElem() {
-        return $('input[type=submit]');
-    }
+  get loginButtonElem() {
+    return $("input[type=submit]");
+  }
 
-    get errorMessageElem() {
-        return $('h3[data-test=error]');
-    }
+  get errorMessageElem() {
+    return $("h3[data-test=error]");
+  }
 
-    open() {
-        browser.url('/');
-    }
+  open() {
+    browser.url("/");
+  }
 
-    isErrorMessagePresent() {
-        return this.errorMessageElem.isDisplayed();
-    }
+  isErrorMessagePresent() {
+    return this.errorMessageElem.isDisplayed();
+  }
 
-    loginWithStandardUser() {
-        this.usernameElem.addValue("standard_user");
-        this.passwordElem.addValue("secret_sauce");
-        this.loginButtonElem.click();
-    }
+  loginWithStandardUser() {
+    this.usernameElem.addValue("standard_user");
+    this.passwordElem.addValue("secret_sauce");
+    this.loginButtonElem.click();
+  }
 
-    loginWithBadUser() {
-        this.usernameElem.addValue('badUser');
-        this.passwordElem.addValue("secret_sauce");
-        this.loginButtonElem.click();
-    }
+  loginWithBadUser() {
+    this.usernameElem.addValue("badUser");
+    this.passwordElem.addValue("secret_sauce");
+    this.loginButtonElem.click();
+  }
 
-    loginWithBadPassword() {
-        this.usernameElem.addValue('standard_user');
-        this.passwordElem.addValue("badPassword");
-        this.loginButtonElem.click();
-    }
+  loginWithBadPassword() {
+    this.usernameElem.addValue("standard_user");
+    this.passwordElem.addValue("badPassword");
+    this.loginButtonElem.click();
+  }
 }
 
 module.exports = new LoginPage();
--- test/specs/login.spec.js
+++ test/specs/login.spec.js
@@ -1,23 +1,23 @@
 const put = require("../pages/login.page.js");
-const expect = require('chai').expect;
+const expect = require("chai").expect;
 
-describe('Login -- Brandon Gibbons', () => {
-    beforeEach(() => {
-        put.open();
-    });
+describe("Login -- Brandon Gibbons", () => {
+  beforeEach(() => {
+    put.open();
+  });
 
-    it('should be able to login with the standard user', () => {
-        put.loginWithStandardUser();
-        expect(browser.getUrl()).to.include('/inventory.html');
-    });
+  it("should be able to login with the standard user", () => {
+    put.loginWithStandardUser();
+    expect(browser.getUrl()).to.include("/inventory.html");
+  });
 
-    it('should throw error to login with an invalid user', () => {
-        put.loginWithBadUser();
-        expect(put.isErrorMessagePresent()).to.equal(true);
-    });
+  it("should throw error to login with an invalid user", () => {
+    put.loginWithBadUser();
+    expect(put.isErrorMessagePresent()).to.equal(true);
+  });
 
-    it('should throw error to login with an invalid password', () => {
-        put.loginWithBadPassword();
-        expect(put.isErrorMessagePresent()).to.equal(true);
-    });
+  it("should throw error to login with an invalid password", () => {
+    put.loginWithBadPassword();
+    expect(put.isErrorMessagePresent()).to.equal(true);
+  });
 });
